05d2d2de454e2fa039268e02f5c4fcdcb1eef1fe
Saranaveed4 <saranaveed307@gmail.com>
2024-07-06 22:03:18 +0500

    Initial commit

---
+++ student.js
@@ -0,0 +1,46 @@
+class school {
+    addstudent(stuobj) {
+        this.student.push(stuobj);
+    }
+    addTeacher(teachobj) {
+        this.Teachers.push(teachobj);
+    }
+    constructor(name) {
+        this.student = [];
+        this.Teachers = [];
+        this.name = name;
+    }
+}
+class student {
+    constructor(name, age, schoolname, Gender) {
+        this.name = name;
+        this.age = age;
+        this.schoolname = schoolname;
+        this.Gender = Gender;
+    }
+}
+class Teacher extends student {
+}
+let school1 = new school("Alpha");
+let school2 = new school("Hyphen");
+let s1 = new student("Naveed", 19, school2.name, "male");
+let s2 = new student("Tooba", 12, school1.name, "Female");
+let s3 = new student("saaad", 16, school2.name, "male");
+let s4 = new student("Humna", 18, school1.name, "Female");
+let t1 = new Teacher("Sara", 20, school1.name, "Female");
+let t2 = new Teacher("Tahreem", 24, school1.name, "Female");
+let t3 = new Teacher("salman", 32, school2.name, "male");
+let t4 = new Teacher("ali", 40, school1.name, "male");
+let t5 = new Teacher("Ayesha", 22, school2.name, "Female");
+school2.addstudent(s1);
+school1.addstudent(s2);
+school2.addstudent(s3);
+school1.addstudent(s4);
+school1.addTeacher(t1);
+school1.addTeacher(t2);
+school2.addTeacher(t3);
+school1.addTeacher(t4);
+school2.addTeacher(t5);
+console.log(school1);
+console.log(school2);
+export {};
